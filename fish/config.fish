9816dbda23a64ceedc0fe3cedca41348fee5088c
Ryan Baker <ryan.baker@adhocteam.us>
2022-03-01 10:55:10 -0800

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,28 @@
-source $HOME/.config/fish/colors.fish
+# disable startup message
+set fish_greeting
 
-/opt/homebrew/bin/brew shellenv | source
-
-set fish_greeting # disable startup message
-
+# force truecolor
 set -g fish_term24bit 1 # force truecolor
 
+# set colorscheme
+source $HOME/.config/fish/colors.fish
 set __fish_git_prompt_show_informative_status true
 set -gx LSCOLORS ExFxCxDxBxegedabagacad
 
-set -gx fish_user_paths $HOME/.nvim-nightly/nvim-osx64/bin /Applications/Postgres.app/Contents/Versions/latest/bin
+# setup homebrew paths
+/opt/homebrew/bin/brew shellenv | source
 
-" alias vim="nvim"
-" set -gx EDITOR nvim
+# setup asdf paths
+source /usr/local/opt/asdf/libexec/asdf.fish
+
+# setup custom paths
+fish_add_path $HOME/.nvim-nightly/nvim-osx64/bin
+fish_add_path /Applications/Postgres.app/Contents/Versions/latest/bin
+fish_add_path /opt/homebrew/opt/curl/bin
+
+# Use neovim instead of vim
+alias vim="nvim"
+set -gx EDITOR nvim
 
 # don't copy garbage into tar files
 set -gx COPYFILE_DISABLED 1
@@ -20,19 +30,11 @@ set -gx COPYFILE_DISABLED 1
 # homebrew config
 set -gx HOMEBREW_FORCE_BREWED_GIT 1
 set -gx HOMEBREW_BUNDLE_NO_LOCK true
-set -gx HOMEBREW_BUNDLE_FILE "$HOME/.config/brew/Brewfile"
 
-set -gx GOPATH "$HOME/.go"
-
-fish_add_path /usr/local/opt/curl/bin
-
-source /usr/local/opt/asdf/libexec/asdf.fish
-
-# per-system config
+# load per-system config
 source $HOME/.config/fish/local.fish
 
 if status is-interactive
 and not set -q TMUX
     tmux new-session -A -s 0
 end
-
