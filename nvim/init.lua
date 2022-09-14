bffb73dd2f92f550cfde79c305e3560d5fb34ad7
Ryan Baker <ryan.baker@adhocteam.us>
2022-09-14 14:23:50 -0700

    neovim config cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,7 +32,7 @@ vim.opt.softtabstop = -1
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.tabstop = 4
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = 'no'
 vim.opt.wildmode = {'longest:full', 'full'}
 vim.opt.termguicolors = true
 vim.opt.background = 'dark'
@@ -43,10 +43,10 @@ vim.g.gruvbox_sign_column = 'bg1'
 vim.g.gruvbox_invert_tabline = 1
 vim.g.zig_fmt_autosave = 0
 
-require("gruvbox").setup({
+require('gruvbox').setup({
   italic = false,
   overrides = {
-    SignColumn = { bg = "#504945" }         
+    SignColumn = { bg = '#504945' }         
   }
 })
 vim.cmd [[colorscheme gruvbox]]
@@ -57,26 +57,29 @@ vim.api.nvim_create_autocmd('FileType', {pattern='*', command='set formatoptions
 --- compile packer configuration after saving init.lua
 vim.api.nvim_create_autocmd('BufWritePost', { pattern={'init.lua'}, command='PackerCompile' })
 
+local opts = { noremap=true, silent=true }
+function merge(...) return vim.tbl_extend('force', ...) end
+
 --- close all helper windows
-vim.api.nvim_set_keymap('n', '<Leader>q', ':pclose <Bar> cclose <Bar> lclose <Bar> helpclose<CR>', {noremap=true, silent=true})
+vim.keymap.set('n', '<Leader>q', ':pclose <Bar> cclose <Bar> lclose <Bar> helpclose<CR>', opts)
 
 -- telescope config
-local tele = require'telescope.builtin'
-vim.api.nvim_set_keymap('n', '<Leader>f', '', { callback=tele.find_files, noremap=true, silent=true })
-vim.api.nvim_set_keymap('n', '<Leader><Leader>', '', { callback=tele.buffers, noremap=true, silent=true })
-vim.api.nvim_set_keymap('n', '<Leader>/', '', { callback=tele.live_grep, noremap=true, silent=true })
+local tele = require('telescope.builtin')
+vim.keymap.set('n', '<Leader>f', '', merge(opts, { callback=tele.find_files }))
+vim.keymap.set('n', '<Leader><Leader>', '', merge(opts, { callback=tele.buffers}))
+vim.keymap.set('n', '<Leader>/', '', merge(opts, { callback=tele.live_grep }))
 
 local lspconfig = require('lspconfig')
--- local opts = { noremap=true, silent=true }
 -- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 -- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 -- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 -- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.api.nvim_win_set_option(0, 'signcolumn', 'yes')
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  local bufopts = merge(opts, { buffer=bufnr })
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
