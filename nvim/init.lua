30d8e94bdf9762c0ef3fd81e391af268a3d88ada
Ryan Baker <mail@rlbaker.net>
2022-11-30 10:50:11 -0800

    nvim updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,6 +17,7 @@ require('packer').startup(function(use)
   use 'ellisonleao/gruvbox.nvim'
   use 'tpope/vim-fugitive'
   use 'tpope/vim-commentary'
+  use 'axelf4/vim-strip-trailing-whitespace'
   use 'neovim/nvim-lspconfig'
   use 'blankname/vim-fish'
   use {
@@ -32,6 +33,9 @@ require('packer').startup(function(use)
     end,
   }
 
+  use 'Olical/conjure'
+  use 'gpanders/nvim-parinfer'
+
   if packer_bootstrap then
     require('packer').sync()
   end
@@ -45,6 +49,7 @@ vim.opt.confirm = true
 vim.opt.cursorline = true
 vim.opt.expandtab = true
 vim.opt.ignorecase = true
+vim.opt.laststatus = 3
 vim.opt.mouse = 'a'
 vim.opt.shiftwidth = 0
 vim.opt.shortmess:append 'cI'
@@ -57,28 +62,41 @@ vim.opt.tabstop = 4
 vim.opt.signcolumn = 'no'
 vim.opt.wildmode = {'longest:full', 'full'}
 vim.opt.termguicolors = true
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader=','
 vim.g.html_indent_autotags = 'html,head,body'
 vim.g.no_ocaml_maps = true
+-- vim.g['conjure#log#wrap'] = true
+-- vim.g['conjure#log#hud#height'] = 0.5
+-- vim.g['conjure#log#break_length'] = 40
 
 require('gruvbox').setup({
   italic = false,
   overrides = {
     SignColumn = { bg = '#504945' },
-    NormalFloat = { bg = '#32302f' }
+    NormalFloat = { bg = '#504945' },
   }
 })
 vim.cmd [[colorscheme gruvbox]]
 
 --- disable comment continuations
-vim.api.nvim_create_autocmd('FileType', { pattern='*', command='set formatoptions-=c' })
-vim.api.nvim_create_autocmd('FileType', { pattern='css,html,javascript,json,lua,ocaml', command='set tabstop=2' })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = '*',
+  -- command = 'set formatoptions-=c'
+  callback = function() vim.opt.formatoptions:remove 'c' end
+})
+
+-- 2 space indents for some languages
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'css,html,javascript,json,lua,ocaml',
+  callback = function() vim.opt.tabstop = 2 end
+})
 
 -- treesitter
 require('nvim-treesitter.configs').setup {
   auto_install = false,
-  ensure_installed = { 'go', 'help', 'lua', 'ocaml', 'vim' },
+  ensure_installed = { 'clojure', 'go', 'help', 'lua', 'ocaml', 'vim' },
   highlight = { enable = true }
 }
 
@@ -100,7 +118,7 @@ require('telescope').setup {
   }
 }
 
-local opts = { noremap=true, silent=true }
+local opts = { silent=true }
 
 local telescope = require('telescope.builtin')
 --- close all helper windows
@@ -113,26 +131,39 @@ vim.keymap.set('n', '<Leader>c', telescope.commands, opts)
 
 -- lsp specific mappings
 vim.keymap.set('n', '<LocalLeader>/', telescope.current_buffer_fuzzy_find)
-vim.keymap.set('n', '<LocalLeader>s', telescope.lsp_document_symbols, opts)
-vim.keymap.set('n', '<LocalLeader>S', telescope.lsp_dynamic_workspace_symbols, opts)
 vim.keymap.set('n', '<LocalLeader>q', telescope.diagnostics, opts)
 vim.keymap.set('n', '<LocalLeader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
+-- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+--     border = "single"
+-- })
+
 -- lsp
 local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  local bufopts = { silent=true, buffer=bufnr }
+
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+
   vim.keymap.set('n', '<LocalLeader>d', telescope.lsp_definitions, bufopts)
-  vim.keymap.set('n', '<LocalLeader>r', telescope.lsp_references, bufopts)
   vim.keymap.set('n', '<LocalLeader>D', vim.lsp.buf.declaration, bufopts)
+
+  vim.keymap.set('n', '<LocalLeader>r', telescope.lsp_references, bufopts)
   vim.keymap.set('n', '<LocalLeader>R', vim.lsp.buf.rename, bufopts)
+
+  vim.keymap.set('n', '<LocalLeader>s', telescope.lsp_document_symbols, opts)
+  vim.keymap.set('n', '<LocalLeader>S', telescope.lsp_dynamic_workspace_symbols, opts)
+
   vim.keymap.set('n', '<LocalLeader>i', telescope.lsp_implementations, bufopts)
+
   vim.keymap.set('n', '<LocalLeader>a', vim.lsp.buf.code_action, bufopts)
+
   vim.keymap.set('n', '<LocalLeader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+
   vim.keymap.set('n', '<LocalLeader>o', function()
     vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
   end, bufopts)
@@ -140,15 +171,17 @@ end
 
 require('lspconfig').ocamllsp.setup {
   on_attach = on_attach,
-  single_file_support = true
+  single_file_support = true,
 }
+
 
 require('lspconfig').gopls.setup {
   on_attach = on_attach,
   settings = {
     gopls = {
-      gofumpt = true, 
+      gofumpt = true,
       staticcheck = true,
     }
   }
 }
+
