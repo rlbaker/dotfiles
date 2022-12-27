8dfb5b5f69e675274ee4013f9c1206c32c15eed0
Ryan Baker <rlbaker@users.noreply.github.com>
2022-12-27 13:31:30 -0800

    Update plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -40,44 +40,14 @@ require('packer').startup(function(use)
   }
   use 'nvim-telescope/telescope-ui-select.nvim'
 
-  use 'clojure-vim/clojure.vim'
   use 'luochen1990/rainbow'
-  use 'guns/vim-sexp'
-  use 'tpope/vim-sexp-mappings-for-regular-people'
-  
-  use {
-    'Olical/conjure',
+  use 'clojure-vim/clojure.vim'
+  use { 'guns/vim-sexp',
     setup = function()
-      vim.g['conjure#client_on_load'] = false
-
-      vim.g['conjure#eval#inline_results'] = false
-      vim.g['conjure#extract#tree_sitter#enabled'] = true
-      vim.g['conjure#filetypes'] = { 'clojure' }
-      vim.g['conjure#client#clojure#nrepl#connection#auto_repl#enabled'] = false
-
-      vim.g['conjure#log#botright'] = true
-      vim.g['conjure#log#hud#height'] = 0.66
-      vim.g['conjure#log#jump_to_latest#cursor_scroll_position'] = 'center'
-      vim.g['conjure#log#jump_to_latest#enabled'] = true
-      vim.g['conjure#log#wrap'] = true
-      
-      -- disable some default mappings
-      vim.g['conjure#mapping#log_split'] = 'cls'
-      vim.g['conjure#mapping#log_vsplit'] = 'clv'
-      vim.g['conjure#mapping#log_tab'] = 'clt'
-      vim.g['conjure#mapping#log_buf'] = 'clb'
-      vim.g['conjure#mapping#log_toggle'] = 'clg'
-      vim.g['conjure#mapping#log_reset_soft'] = 'clr'
-      vim.g['conjure#mapping#log_reset_hard'] = 'clR'
-      vim.g['conjure#mapping#log_jump_to_latest'] = 'cll'
-      vim.g['conjure#mapping#log_close_visible'] = 'clq'
-
-      -- disable features provided by clojure-lsp
-      vim.g['conjure#completion#omnifunc'] = false
-      vim.g['conjure#mapping#def_word'] = false
-      vim.g['conjure#mapping#doc_word'] = false
-    end,
+    end
   }
+  use 'tpope/vim-sexp-mappings-for-regular-people'
+  use 'Olical/conjure'
 
   if packer_bootstrap then
     require('packer').sync()
