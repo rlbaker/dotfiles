dfddb0f142744c0bd98e046a7a8fdd49e0da4817
Ryan Baker <mail@rlbaker.net>
2023-01-18 10:54:37 -0800

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -45,8 +45,9 @@ vim.opt.tabstop = 2
 
 vim.g.html_indent_autotags = 'html,head,body'
 
---- disable comment continuations
 vim.api.nvim_create_augroup('rlb', { clear = true })
+
+-- disable comment continuations
 vim.api.nvim_create_autocmd('FileType', {
   pattern = '*', group = 'rlb',
   callback = function()
@@ -54,6 +55,7 @@ vim.api.nvim_create_autocmd('FileType', {
   end
 })
 
+-- highlight clojure symbols
 vim.api.nvim_create_autocmd('Filetype', {
   pattern = 'clojure', group = 'rlb',
   callback = function()
@@ -61,6 +63,7 @@ vim.api.nvim_create_autocmd('Filetype', {
   end
 })
 
+-- disable lua table bracket highlighting
 vim.api.nvim_create_autocmd('Filetype', {
   pattern = 'lua', group = 'rlb',
   callback = function()
@@ -68,14 +71,16 @@ vim.api.nvim_create_autocmd('Filetype', {
   end
 })
 
+-- auto-trim whitespace
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*', group = 'rlb',
   command = [[%s/\s\+$//e]],
 })
 
+--- Keybindings
 local opts = { noremap = true, silent = true }
 
---- close all helper windows
+-- close all helper windows
 vim.keymap.set('n', '<Leader>q', function()
   vim.cmd('pclose')
   vim.cmd('cclose')
@@ -97,4 +102,83 @@ vim.keymap.set('n', '<Leader>[', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', '<Leader>]', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<LocalLeader>/', telescope.current_buffer_fuzzy_find, opts)
 
-require('lsp')
+--- LSP Configuration
+
+local function on_attach(client, bufnr)
+  client.server_capabilities.semanticTokensProvider = nil
+  -- vim.lsp.codelens.refresh()
+
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+
+  vim.keymap.set('n', 'gd', telescope.lsp_definitions, bufopts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gr', telescope.lsp_references, bufopts)
+  vim.keymap.set('n', 'gi', telescope.lsp_implementations, bufopts)
+
+  vim.keymap.set('n', 'gR', vim.lsp.buf.rename, bufopts)
+
+  vim.keymap.set('n', '<LocalLeader>ci', telescope.lsp_incoming_calls, bufopts)
+  vim.keymap.set('n', '<LocalLeader>co', telescope.lsp_outgoing_calls, bufopts)
+
+  vim.keymap.set('n', '<LocalLeader>s', telescope.lsp_document_symbols, bufopts)
+  vim.keymap.set('n', '<LocalLeader>S', telescope.lsp_dynamic_workspace_symbols, bufopts)
+
+  vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gf', function() vim.lsp.buf.format { async = true } end, bufopts)
+  vim.keymap.set('n', 'go', function()
+    vim.lsp.buf.code_action({
+      context = { only = { 'source.organizeImports' } },
+      apply = true
+    })
+  end, bufopts)
+end
+
+require('lspconfig').gopls.setup {
+  on_attach = on_attach,
+  settings = {
+    gopls = {
+      gofumpt = true,
+      staticcheck = true,
+    }
+  }
+}
+
+require('lspconfig').sumneko_lua.setup {
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      completion = { keywordSnippet = 'Disable' },
+      diagnostics = { globals = { 'vim', 'love' } },
+      runtime = { version = 'LuaJIT' },
+      semantic = { enable = false },
+      telemetry = { enable = false },
+      format = {
+        enable = true,
+        defaultConfig = {
+          indent_size = '2',
+          indent_style = 'space',
+          align_call_args = 'true',
+          continuation_indent_size = '2',
+          -- if_condition_align_with_each_other = 'true',
+          if_condition_no_continuation_indent = 'true',
+          local_assign_continuation_align_to_first_expression = 'true',
+          quote_style = 'single',
+          table_append_expression_no_space = 'true',
+        },
+      }
+    },
+  },
+}
+
+require('lspconfig').clojure_lsp.setup {
+  on_attach = on_attach,
+  root_dir = function(fname)
+    -- prevent LSP from attaching to conjure buffer
+    if string.match(fname, 'conjure%-log%-%d+') then return nil end
+    return require('lspconfig.util').root_pattern('deps.edn', 'bb.edn', '.git')(fname)
+  end,
+}
