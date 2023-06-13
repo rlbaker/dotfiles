99af90e86e319f9c0f343c47ef19d5a1d86ac8f0
Ryan Baker <rlbaker@users.noreply.github.com>
2023-06-13 13:48:08 -0700

    Update plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,68 +3,51 @@ return {
         'sainnhe/gruvbox-material',
         lazy = false,
         priority = 1000,
-        config = function()
+        init = function()
             vim.g.gruvbox_material_better_performance = 1
             vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
             vim.cmd([[colorscheme gruvbox-material]])
             vim.api.nvim_set_hl(0, 'MatchParen', { fg = '#FF0000' })
-        end
+        end,
     },
 
     { 'neovim/nvim-lspconfig' },
-
+    { 'tpope/vim-repeat' },
+    { 'tpope/vim-commentary' },
+    { 'tpope/vim-surround' },
+    { 'tpope/vim-fugitive', cmd = 'Git' },
     {
-        'nvim-lualine/lualine.nvim',
-        opts = {
-            tabline = {
-                lualine_a = { 'buffers' },
-                lualine_z = { 'tabs' },
-            },
-            options = {
-                icons_enabled = false,
-                section_separators = '',
-                component_separators = '',
-            },
-        },
+        'windwp/nvim-autopairs',
+        event = 'VeryLazy',
+        opts = { check_ts = true },
     },
 
     {
         'nvim-treesitter/nvim-treesitter',
+        event = 'VeryLazy',
+        dependencies = { 'RRethy/nvim-treesitter-endwise' },
         build = function()
             local update = require('nvim-treesitter.install').update { with_sync = true }
             update()
         end,
-        dependencies = { 'RRethy/nvim-treesitter-endwise' },
         config = function()
             require('nvim-treesitter.configs').setup {
                 auto_install = false,
-                ensure_installed = {
-                    'bash',
-                    'fish',
-                    'go',
-                    'lua',
-                    'vim',
-                    'vimdoc',
-                },
+                ensure_installed = { 'bash', 'fish', 'go', 'lua', 'vim', 'vimdoc' },
                 highlight = { enable = true },
+                indent = { enable = true },
                 endwise = { enable = true },
             }
-        end
-    },
-
-    {
-        'windwp/nvim-autopairs',
-        opts = {
-            check_ts = true
-        }
+        end,
     },
 
     {
         'nvim-telescope/telescope.nvim',
+        lazy = true,
         branch = '0.1.x',
         dependencies = {
             'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope-ui-select.nvim'
+            'nvim-telescope/telescope-ui-select.nvim',
         },
         config = function()
             local telescope = require('telescope')
@@ -77,19 +60,9 @@ return {
                     },
                     sorting_strategy = 'ascending',
                 },
-                pickers = {
-                    buffers = { sort_lastused = true },
-                },
+                pickers = { buffers = { sort_lastused = true } },
             }
             telescope.load_extension('ui-select')
-        end
-    },
-
-    { 'tpope/vim-repeat' },
-    { 'tpope/vim-commentary' },
-    { 'tpope/vim-surround' },
-    {
-        'tpope/vim-fugitive',
-        cmd = 'Git'
+        end,
     },
 }
