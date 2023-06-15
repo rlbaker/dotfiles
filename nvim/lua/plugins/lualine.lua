874d571d10d84f63b90a2d8437d4dfa33bc097c4
Ryan Baker <mail@rlbaker.net>
2023-06-14 19:41:21 -0700

    add lualine

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+        options = {
+            icons_enabled = false,
+            section_separators = '',
+            component_separators = '',
+            globalstatus = true,
+        },
+    },
+}
