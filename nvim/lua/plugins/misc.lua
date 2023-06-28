29f04d447abf82a5c400aa148924e3ca9bb814b2
Ryan Baker <mail@rlbaker.net>
2023-06-28 08:29:15 -0700

    cleanup

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -2,18 +2,4 @@ return {
   { 'vmchale/just-vim', ft = 'just' },
   { 'tpope/vim-fugitive', cmd = 'Git' },
   { 'tpope/vim-commentary', event = 'VeryLazy' },
-  -- { 'windwp/nvim-autopairs', event = 'InsertEnter', opts = { check_ts = true } },
-  -- {
-  --   'echasnovski/mini.completion',
-  --   -- event = 'LspAttach',
-  --   opts = {
-  --     -- lsp_completion = { source_func = 'omnifunc', auto_setup = false },
-  --     delay = { completion = 10000000, signature = 100, info = 100 },
-  --     fallback_action = nil,
-  --     -- mappings = {
-  --     --   force_twostep = '<C-Space>',
-  --     --   force_fallback = '',
-  --     -- },
-  --   },
-  -- },
 }
