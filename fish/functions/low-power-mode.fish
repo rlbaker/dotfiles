8d5611e8b953e82c2db9568c463cb316aed05980
Ryan Baker <mail@rlbaker.net>
2023-09-17 13:55:22 -0700

    log current powermode value

--- fish/functions/low-power-mode.fish
+++ fish/functions/low-power-mode.fish
@@ -2,11 +2,11 @@ function low-power-mode
     switch $argv[1]
         case 'on'
             sudo pmset -b lowpowermode 1
-            echo 'low power mode enabled'
         case 'off'
             sudo pmset -b lowpowermode 0
-            echo 'low power mode disabled'
         case '*'
             echo 'valid values: on | off'
+            echo '----'
     end
+    pmset -g custom | rg --color=never '(^Battery)|(^AC)| lowpower'
 end
