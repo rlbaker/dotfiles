dd83562ac68fb01afbcbc86087d6e80f8f5061cc
Ryan Baker <git@rlbaker.net>
2023-10-14 12:34:43 -0700

    fish function updates

--- fish/functions/update-zig.fish
+++ fish/functions/update-zig.fish
@@ -1,6 +1,5 @@
 function update-zig
-    echo "updating zig compiler"
     asdf uninstall zig master
-    and asdf install zig master
-    and asdf global zig master
+    asdf install zig master
+    asdf global zig master
 end
--- fish/functions/update-zls.fish
+++ fish/functions/update-zls.fish
@@ -1,5 +1,4 @@
 function update-zls
-    echo "updating zig language server"
     pushd ~/src/zig/zls
     git pull
     zig build -Doptimize=ReleaseSafe -Dlog_level=warn
