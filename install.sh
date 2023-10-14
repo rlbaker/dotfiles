e0cb1777adce0ea2f0dfb6b5b2a49c859146e73b
Ryan Baker <git@rlbaker.net>
2023-10-13 18:27:18 -0700

    update install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+set -euo pipefail
+
+CONFIG_DIR="$HOME/.config"
+
+case $1 in
+    nvim)
+        mkdir -p "$CONFIG_DIR/nvim"
+        mkdir -p "$CONFIG_DIR/nvim/lua/plugins"
+        fd --type file --extension lua . nvim --exec ln -vs "$(pwd)/{}" "$HOME/.config/{}"
+        ;;
+    fish)
+        mkdir -p "$CONFIG_DIR/fish"
+        mkdir -p "$CONFIG_DIR/fish/functions"
+        touch "$CONFIG_DIR/fish/local.fish"
+        fd --type file --extension fish . fish --exec ln -vs "$(pwd)/{}" "$CONFIG_DIR/{}"
+        ;;
+    kitty)
+        mkdir -p "$CONFIG_DIR/kitty"
+        fd --type file . kitty --exec ln -vs "$(pwd)/{}" "$CONFIG_DIR/{}"
+        ;;
+    homebrew)
+        mkdir -p "$CONFIG_DIR/homebrew"
+        fd --type file . homebrew --exec ln -vs "$(pwd)/{}" "$CONFIG_DIR/{}"
+esac
