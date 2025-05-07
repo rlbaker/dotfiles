aa308e7bc3b01fb7fb7798202d8fe7589eecd6b8
Ryan Baker <git@rlbaker.net>
2025-05-07 10:27:34 -0700

    refactor: Conditionally include config and models files in fish script

--- fish/functions/aider.fish
+++ fish/functions/aider.fish
@@ -5,13 +5,18 @@ function aider
         return 1
     end
 
+    set -l aider_args
     set -l config_file "$HOME/.config/aider/aider.conf.yml"
     set -l models_file "$HOME/.config/aider/aider.model.settings.yml"
 
+    if test -f "$config_file"
+        set -a aider_args --config "$config_file"
+    end
+
+    if test -f "$models_file"
+        set -a aider_args --model-settings-file "$models_file"
+    end
+
     # Execute the aider command with the specified config file and pass through all other arguments
-    command aider \
-        --config "$config_file" \
-        --model-settings-file "$models_file" \
-        --no-show-model-warnings \
-        $argv
+    command aider $aider_args --no-show-model-warnings $argv
 end
