7272143e2605f5332cfd8cfbda2aaad946222d0c
Ryan Baker <git@rlbaker.net>
2025-06-13 16:40:00 -0700

    refactor: clean up key mappings and improve fish config sourcing logic

--- fish/config.fish
+++ fish/config.fish
@@ -40,3 +40,6 @@ else
 end
 
 source $HOME/.config/fish/local.fish # load system-specific configuration
+
+
+test -r '/Users/rlbaker/.opam/opam-init/init.fish' && source '/Users/rlbaker/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
